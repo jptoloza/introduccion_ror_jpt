d2bfa892179adf822e7f7c54b67f24dd0f30441c
Jorge Ponce T <jptoloza@gmail.com>
2019-07-23 04:33:12 -0400

    Commit update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,11 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+
+  config.action_mailer.default_url_options = { :host => ENV['MAILER_URL'] }
+
+  #heroku config:set MAILER_URL=my-awesome-app.herokuapp.com --app my-awesome-app
+  
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
