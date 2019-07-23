627b3d360562147cefb44f79e04c86e71f8bf7da
Jorge Ponce T <jptoloza@gmail.com>
2019-07-23 03:04:28 -0400

    heroku update

--- db/migrate/20190723042748_add_name_to_users.rb
+++ db/migrate/20190723042748_add_name_to_users.rb
@@ -1,7 +1,7 @@
 class AddNameToUsers < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :first_name, :string
-    add_column :users, :lastname_name, :string
+    add_column :users, :last_name, :string
     add_column :users, :username, :string
     add_column :users, :confirmation_token, :string
     add_column :users, :confirmed_at, :datetime
--- db/migrate/20190723051853_modify_lastname_user.rb
+++
@@ -1,5 +0,0 @@
-class ModifyLastnameUser < ActiveRecord::Migration[5.2]
-	def change
-		rename_column :users, :lastname_name, :last_name
-	end
-end
