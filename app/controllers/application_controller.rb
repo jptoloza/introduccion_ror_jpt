0fde428b1e4dbfcb8393d392cb6e74156d7d92e1
Jorge Ponce T <jptoloza@gmail.com>
2019-07-24 04:14:35 -0400

    agregar no-cache

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,10 @@ class ApplicationController < ActionController::Base
 			user.permit(:first_name, :last_name, :email, :password, :password_confirmation)
 		end
 	end
+
+	def set_cache_headers
+		response.headers["Cache-Control"] = "no-cache, no-store"
+		response.headers["Pragma"] = "no-cache"
+		response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
+	end
 end
